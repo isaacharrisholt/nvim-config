76dbdda29a5f0bdca72e2bc3c610d9da66358cd4
Isaac Harris-Holt <isaac@harris-holt.com>
2024-05-30 12:08:14 +0100

    try supermaven

---
+++ lua/plugins/supermaven.lua
@@ -0,0 +1,10 @@
+return {
+  "supermaven-inc/supermaven-nvim",
+  config = function()
+    require("supermaven-nvim").setup({
+      keymaps = {
+        accept_suggestion = "<Tab>",
+      },
+    })
+  end,
+}
