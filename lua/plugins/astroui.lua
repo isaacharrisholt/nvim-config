6bc9cb5276615a69ecf47a94bacc8230fd285048
Isaac Harris-Holt <isaac@harris-holt.com>
2024-09-07 22:39:05 +0100

    fix zig

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -1,4 +1,4 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroUI provides the basis for configuring the AstroNvim User Interface
 -- Configuration documentation can be found with `:h astroui`
@@ -17,7 +17,7 @@ return {
       init = { -- this table overrides highlights in all themes
         -- Normal = { bg = "#000000" },
       },
-      astrotheme = { -- a table of overrides/changes when applying the astrotheme theme
+      astrodark = { -- a table of overrides/changes when applying the astrotheme theme
         -- Normal = { bg = "#000000" },
       },
     },
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,20 +5,23 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+  opts = {
+    ensure_installed = {
       "lua",
       "vim",
+      "zig",
       "gleam",
       "rust",
       "typescript",
       "svelte",
       "python",
+      "toml",
       "json",
       "javascript",
       "html",
-      -- add more arguments for adding more treesitter parsers
-    })
-  end,
+      "css",
+      "markdown",
+      "yaml",
+    },
+  },
 }
